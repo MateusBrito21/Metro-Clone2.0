a87d483f9d8aded3bcffa57cee5fd1a3d01f14b0
MateusBrito21 <71047149+MateusBrito21@users.noreply.github.com>
2024-01-22 20:43:55 -0300

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -23,25 +24,50 @@ public class PlayerController : MonoBehaviour
     public float dashSpeed, dashTime;
     private float dashCounter;
 
+    public SpriteRenderer theSR, afterImage;
+    public float afterImageLifetime, timeBetweenAfterImages;
+    private float afterImageCounter;
+    public Color afterImageColor;
 
+    public float waitAfterDashing;
+    private float dashRechargeCounter;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame (Dash)
     void Update()
     {
-        if(Input.GetButtonDown("Fire2"))
+        if(dashRechargeCounter > 0)
         {
-            dashCounter = dashTime;
+            dashRechargeCounter -= Time.deltaTime;
         }
+        else
+        {
+
+            if(Input.GetButtonDown("Fire2"))
+            {
+                dashCounter = dashTime;
+
+                ShowAfterimage();
+            }
+        } 
+
         if(dashCounter >0)
         {
             dashCounter = dashCounter - Time.deltaTime;
 
             theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
+
+            afterImageCounter -= Time.deltaTime;
+            if(afterImageCounter <=0)
+            {
+                ShowAfterimage();
+            }
+
+            dashRechargeCounter = waitAfterDashing;
         }
         else
         {
@@ -102,5 +128,18 @@ public class PlayerController : MonoBehaviour
         //Abs = absolute value of something
         anim.SetBool("isOnGround", isOnGround);
         anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+    }
+
+    public void ShowAfterimage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = theSR.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+        
+        Destroy(image.gameObject, afterImageLifetime);
+
+        afterImageCounter = timeBetweenAfterImages;
+
     }
 }
