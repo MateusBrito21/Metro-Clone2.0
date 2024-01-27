94113436dd7791764ddae1b69888dc7e4e2dd0be
Mateus Brito <71047149+MateusBrito21@users.noreply.github.com>
2024-01-26 21:28:54 -0300

    Add files via upload

---
+++ AbilityUnlock.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AbilityUnlock : MonoBehaviour
+{
+    public bool unlockDoubleJump, unlockDash, unlockBecomBall, unlockDropBomb;
+
+    public GameObject pickupEffect;
+    
+    public string unlockMassage;
+    public TMP_Text unlockText;
+
+   
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+            PlayerAbilityTracker player = other.GetComponentInParent<PlayerAbilityTracker>();
+
+            if(unlockDoubleJump)
+            {
+                player.canDoubleJump = true;
+            }
+
+            if(unlockDash)
+            {
+                player.canDash = true;
+            }
+
+            if(unlockBecomBall)
+            {
+                player.canBecomeBall = true;
+            }
+
+            if(unlockDropBomb)
+            {
+                player.canDropBomb = true;
+            }
+
+            Instantiate(pickupEffect,transform.position,transform.rotation);
+            
+            unlockText.transform.parent.SetParent(null);
+            unlockText.transform.parent.position = transform.position;
+
+            unlockText.text = unlockMassage;
+            unlockText.gameObject.SetActive(true);
+
+            Destroy(unlockText.transform.parent.gameObject,5f);
+
+            Destroy(gameObject);
+        }
+    }
+}
