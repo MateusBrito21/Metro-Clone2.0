09331f8a6b1ba015e5db880253e522765fb595ef
Mateus Brito <71047149+MateusBrito21@users.noreply.github.com>
2024-02-01 08:26:50 -0300

    Add files via upload

--- MainMenu.cs
+++ MainMenu.cs
@@ -7,14 +7,32 @@ public class MainMenu : MonoBehaviour
 {
     public string newGameScene;
 
+    public GameObject conttinueButton;
+
+    public PlayerAbilityTracker player;
+
     void Start()
     {
+        if(PlayerPrefs.HasKey("ContinueLevel"))
+        {
+            conttinueButton.SetActive(true);
+        }
+
         AudioManager.instance.PlayMainMenuMusic();
     }
     public void NewGame()
     {
+        PlayerPrefs.DeleteAll();
         SceneManager.LoadScene(newGameScene);
 
+    }
+
+    public void Continue()
+    {
+        player.gameObject.SetActive(true);
+        player.transform.position = new Vector3(PlayerPrefs.GetFloat("PosX"),PlayerPrefs.GetFloat("PosY"),PlayerPrefs.GetFloat("PosZ"));
+        
+        SceneManager.LoadScene(PlayerPrefs.GetString("ContinueLevel"));
     }
 
     public void QuitGame()
